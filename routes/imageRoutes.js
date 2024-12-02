284afb8ddca79553ac2c302b25c985ea1cf6f1ec
Rachit2833 <rrachit346@gmail.com>
2024-12-02 17:26:24 +0530

    updated Image Schema and Favourite routes

--- routes/imageRoutes.js
+++ routes/imageRoutes.js
@@ -1,11 +1,11 @@
 const express = require("express")
-const { addNewImage,  getAllImages, getAllImagesForLocation, getAllLocations, test, getImageById, deleteImage } = require("../controller/imageController")
+const { addNewImage,  getAllImages, getAllImagesForLocation, getAllLocations, test, getImageById, deleteImage, setAndUnsetFavourite } = require("../controller/imageController")
 const {  searchThrottle } = require("../middleware/imageMiddleWare")
 const router = express.Router()
 router.route("/").get(getAllImages).post(addNewImage).delete(deleteImage)
 router.route("/location").get(getAllLocations);
 router.route("/loc").get(getAllImagesForLocation);
-router.route("/:id").get(getImageById);
+router.route("/:id").get(getImageById).patch(setAndUnsetFavourite);
 
 
 
