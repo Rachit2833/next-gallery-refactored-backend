b8b48c930e4b8e8a08c084db7a5f90376dad7b62
Rachit2833 <rrachit346@gmail.com>
2025-07-15 08:58:04 +0530

    New Struct for Vercel Deployement

--- server.js
+++ server.js
@@ -1,21 +1,54 @@
 const express = require("express");
-const http = require("http");
 const dotenv = require("dotenv");
-const mongoose = require("mongoose");
-const app = require("./app");
-const { server } = require("./lib/socket");
+const labelRouter= require("./routes/labelRoutes")
+const imageRouter= require("./routes/imageRoutes")
+const albumRouter= require("./routes/albumRoutes")
+const statsRouter= require('./routes/statsRouter')
+const userRouter = require('./routes/usersRoutes')
+const authRouter = require('./routes/authRouter')
+const friendRouter = require('./routes/friendRouter')
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const http =require("http")
 const { connectDb } = require("./lib/connectDb");
-
+const app = express()
 dotenv.config({ path: "./config.env" });
 
 const PORT = process.env.PORT || 3000; // Default to port 3000 if not defined
-
+const server = http.createServer(app);
 
 
 // Middleware to parse JSON
-app.use(express.json());
 
-// Connect to MongoDB
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
+
+
+app.use(express.json({ limit: "10mb" })); 
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
+
+app.use("/", labelRouter);
+app.use("/user",userRouter );
+app.use("/image", imageRouter);
+app.use("/album", albumRouter);
+app.use("/stats", statsRouter);
+app.use("/login", authRouter);
+app.use("/friends",friendRouter);
+
+
+
+app.all("*",(req,res)=>{
+   res.status(404).json({
+      message:`Cannot Find the ${req.originalUrl} `
+   })
+})
+
+
 async function startServer() {
   try {
     await  connectDb()
@@ -24,15 +57,11 @@ async function startServer() {
     process.exit(1); // Exit the process if the database connection fails
   }
 }
-module.exports={connectDb}
-
-
-
 
 server.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
 
-
-// Start the database connection
 startServer();
+
+module.exports=app
