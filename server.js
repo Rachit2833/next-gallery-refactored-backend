c6f088f534d4e7803b4e47a83f6f31de14f19171
Rachit2833 <rrachit346@gmail.com>
2025-08-23 10:49:03 +0530

    Testing server

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(
 app.use(express.json({ limit: "10mb" })); 
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.get("/", (req, res) => {
-  res.status(200).json({ message: "HELLO WORLD" });
+  res.status(200).json({ message: "HELLO WORLD TEST" });
 });
 
 
