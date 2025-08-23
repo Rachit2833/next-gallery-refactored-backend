6de60478eb294e984e2819b8cee056a109b902a6
Rachit2833 <rrachit346@gmail.com>
2025-08-23 11:23:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:3000", 
-      "https://next-gallery-by-rachit2833.vercel.app"
+      "https://next-gallery-refactored-zxm4.vercel.app"
     ],
     credentials: true,
   })
